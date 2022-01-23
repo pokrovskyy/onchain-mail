4a44f1629f5561a9961081e1a4bc20dfcf77607b
Serg Pokrovskyy <neosoftlviv@gmail.com>
2022-01-23 00:16:16 -0800

    Round of improvements to the contract + added tests

--- test/deploy.js
+++ test/deploy.js
@@ -1,19 +1,71 @@
 const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const { ethers, waffle } = require("hardhat");
+const provider = waffle.provider;
 
 describe("OnChainMail", function () {
-  it("Should return the new greeting once it's changed", async function () {
+  it("Generic test", async function () {
     const OnChainMail = await ethers.getContractFactory("OnChainMail");
-    const greeter = await OnChainMail.deploy("Hello, world!");
-    await greeter.deployed();
+    const mailer = await OnChainMail.deploy();
+    await mailer.deployed();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    // get sample sender and receiver
+    const [sender, recipient] = await ethers.getSigners();
+    let tx;
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    // sender sends email #1 to receiver (10 ETH reward)
+    tx = await mailer
+      .connect(sender)
+      .sendEmail(recipient.address, false, "asdfasdfasdf", {
+        value: ethers.utils.parseEther("10"),
+      });
+    await tx.wait();
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    // reader reads email #1
+    tx = await mailer.connect(recipient).markRead(1);
+    await tx.wait();
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    expect(await provider.getBalance(recipient.address)).to.within(
+      ethers.utils.parseEther("10009"),
+      ethers.utils.parseEther("10010")
+    );
+
+    // recipient purges email #1 from inbox
+    tx = await mailer.connect(recipient).purge(1);
+    await tx.wait();
+
+    expect((await mailer.receivedMailCount(recipient.address))).to.eq(0);
+    expect((await mailer.sentMailCount(sender.address))).to.eq(1);
+
+    // sender purges email #1 from sent mail
+    tx = await mailer.connect(sender).purge(1);
+    await tx.wait();
+
+    expect((await mailer.receivedMailCount(sender.address))).to.eq(0);
+
+    // sender sends another email #2 (100 ETH reward)
+    tx = await mailer
+      .connect(sender)
+      .sendEmail(recipient.address, false, "asdfasdfasdf", {
+        value: ethers.utils.parseEther("100"),
+      });
+    await tx.wait();
+
+    expect((await mailer.receivedMailCount(recipient.address))).to.eq(1);
+    expect((await mailer.sentMailCount(sender.address))).to.eq(1);
+    expect(await provider.getBalance(sender.address)).to.within(
+      ethers.utils.parseEther("9889"),
+      ethers.utils.parseEther("9890")
+    );
+
+    // sender retracts email #2
+    tx = await mailer.connect(sender).retract(2);
+    await tx.wait();
+
+    expect((await mailer.receivedMailCount(recipient.address))).to.eq(0);
+    expect((await mailer.sentMailCount(sender.address))).to.eq(0);
+    expect(await provider.getBalance(sender.address)).to.within(
+      ethers.utils.parseEther("9989"),
+      ethers.utils.parseEther("9990")
+    );
   });
 });
