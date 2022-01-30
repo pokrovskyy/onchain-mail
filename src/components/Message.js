149f5515973a5664a542c5831b6144d16ef75a6c
calebhskim <caleb.hs.kim@gmail.com>
2022-01-29 18:07:11 -0800

    Cache messages

--- src/components/Message.js
+++ src/components/Message.js
@@ -5,20 +5,25 @@ import toast from 'react-hot-toast'
 import Spinner from './Spinner'
 import { getMessageData } from '../utils/metadata'
 
-const Message = ({ mailMetadata, markRead }) => {
+const Message = ({ mailMetadata: { tokenURI, read = false, sender, title = '', incentiveInWei }, markRead }) => {
 	const [isMessageLoading, setIsMessageLoading] = useState(false)
 	const [accepted, setAccepted] = useState({}) // map of <tokenURI, accepted> accepted messages
 	const [messageData, setMessageData] = useState({})
 
 	const fetchMessageData = async () => {
 		console.log('Fetching message...')
-		let messageData = {};
+		let newMessageData = {...messageData};
 
 		setIsMessageLoading(true)
 
 		try {
-			messageData = await getMessageData(mailMetadata.tokenURI)
-			console.log('Message', messageData)
+			let message = await getMessageData(tokenURI)
+
+			newMessageData[tokenURI] = message
+
+			setMessageData(newMessageData)
+
+			console.log('Message:', message)
 		}
 		catch (e) {
 			console.log('Error getting message:', e)
@@ -27,8 +32,6 @@ const Message = ({ mailMetadata, markRead }) => {
 		finally {
 			setIsMessageLoading(false)
 		}
-
-		setMessageData(messageData)
 	}
 
 	const handleAcceptIncentive = (tokenURI) => {
@@ -42,7 +45,7 @@ const Message = ({ mailMetadata, markRead }) => {
 
 		fetchMessageData()
 
-		if (!mailMetadata.read) {
+		if (!read) {
 			markRead()
 		}
 	}
@@ -51,35 +54,35 @@ const Message = ({ mailMetadata, markRead }) => {
 		<div className="bg-white shadow overflow-hidden sm:rounded-lg">
 			<div className="flex justify-between px-4 py-5 sm:px-6">
 				<div>
-					<h3 className="text-lg leading-6 font-medium text-gray-900">Subject: {messageData?.title || (mailMetadata?.read ? '<No Subject>' : '<Pending read>')}</h3>
+					<h3 className="text-lg leading-6 font-medium text-gray-900">Subject: {messageData[tokenURI]?.title || (read ? '<No Subject>' : '<Pending read>')}</h3>
 				</div>
 			</div>
 			<div className="border-t border-gray-200 px-4 py-5 sm:p-0">
 				<dl className="sm:divide-y sm:divide-gray-200">
 					<div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
 						<dt className="text-sm font-medium text-gray-500">From:</dt>
-						<dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{mailMetadata.sender}</dd>
+						<dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{sender}</dd>
 					</div>
 					<div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
 						<dt className="text-sm font-medium text-gray-500">Incentive:</dt>
-						<dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{ethers.utils.formatEther(mailMetadata.incentiveInWei)} ETH</dd>
+						<dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{ethers.utils.formatEther(incentiveInWei)} ETH</dd>
 					</div>
-					{!accepted[mailMetadata.tokenURI] && <div className="flex justify-end py-4 sm:py-5 sm:px-6">
+					{!accepted[tokenURI] && <div className="flex justify-end py-4 sm:py-5 sm:px-6">
 						<button
 							type="button"
 							className="w-full ml-auto inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:w-auto sm:text-sm"
-							onClick={() => handleAcceptIncentive(mailMetadata?.tokenURI || null)}
+							onClick={() => handleAcceptIncentive(tokenURI || null)}
 						>
-							{!mailMetadata?.read ? 'Accept Incentive' : 'Read Message'}
+							{!read ? 'Accept Incentive' : 'Read Message'}
 						</button>
 					</div>}
-					{accepted[mailMetadata.tokenURI] && <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+					{accepted[tokenURI] && <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
 						<dt className="text-sm font-medium text-gray-500">Message</dt>
 						{!isMessageLoading && <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-							{messageData?.content || 'No message present'}
+							{messageData[tokenURI]?.content || 'No message present'}
 						</dd>}
-						{isMessageLoading && <Spinner />}
 						{isMessageLoading && <p>This may take a while...</p>}
+						{isMessageLoading && <Spinner />}
 					</div>}
 				</dl>
 			</div>
